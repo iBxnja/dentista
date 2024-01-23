a7548bb6ecbce2f6906b40c993294426323eb614
benja <benjavallory@hotmail.com>
2024-01-22 22:17:53 -0300

    cambios

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,4 +1,4 @@
-<header class="w-full h-20 bg-purple-300 flex items-center justify-between">
+<header class="w-full h-20 bg-purple-700 flex items-center justify-between">
     <button id="menuAbrir" class="sm:hidden">
         <svg class="h-10 w-10 cel:ml-5"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="4" y1="6" x2="20" y2="6" />  <line x1="4" y1="12" x2="20" y2="12" />  <line x1="4" y1="18" x2="20" y2="18" /></svg>
     </button>
