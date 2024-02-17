b313ccb78a09d0d6303ca4c7c19bbb9d4e82b655
benja <benjavallory@hotmail.com>
2024-02-16 23:16:52 -0300

    asd

--- app/Http/Controllers/ControladorRegister.php
+++ app/Http/Controllers/ControladorRegister.php
@@ -37,28 +37,31 @@ class ControladorRegister extends Controller
                 ],
                 'password' => 'required|confirmed',
             ]);
-    
+
             // Creación de usuario sin encriptar la contraseña
             $user = User::create([
                 'name' => $validatedData['name'],
                 'email' => $validatedData['email'],
                 'password' => $validatedData['password'],
             ]);
-    
+
+            // Almacenar el ID del usuario en la sesión
+            session()->put('usuario_id', $user->id);
+
             // Login del usuario
             auth()->login($user);
-    
+
             // Mensaje de éxito
             Log::info('Registro exitoso. Usuario logueado.', ['user' => $user]);
             $mensaje = 'Te has registrado correctamente';
             session()->flash('mensaje', $mensaje);
-    
+
             // Redirección a la página de inicio
             return redirect()->to('/inicio');
         } catch (\Exception $e) {
             // Capturar cualquier excepción y registrarla para depuración
             Log::error('Error durante el registro: ' . $e->getMessage());
-    
+
             // Redirigir al usuario a una página de error o mostrar un mensaje de error en la página de registro
             return redirect()->back()->withErrors(['error' => 'Hubo un problema durante el registro. Por favor, inténtalo de nuevo.']);
         }
